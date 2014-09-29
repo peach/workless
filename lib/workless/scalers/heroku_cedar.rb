2e4befb9bfffde51a233bf049c01663f9b7137b3
Mark Wilson <mark@wilsonsdev.com>
2014-09-29 16:48:39 -0700

    avoid api calls by utilizing instance variable and rails caching

--- lib/workless/scalers/heroku_cedar.rb
+++ lib/workless/scalers/heroku_cedar.rb
@@ -5,17 +5,31 @@ module Delayed
     module Scaler
       class HerokuCedar < Base
         extend Delayed::Workless::Scaler::HerokuClient
+        @@mutex = Mutex.new
 
         def self.up
-          client.post_ps_scale(ENV['APP_NAME'], 'worker', self.workers_needed) if self.workers_needed > self.min_workers and self.workers < self.workers_needed
+          @@mutex.sychronize do
+            if self.workers_needed > self.min_workers and self.workers < self.workers_needed
+              client.post_ps_scale(ENV['APP_NAME'], 'worker', self.workers_needed) 
+              @workers = self.workers_needed
+            end
         end
 
         def self.down
-          client.post_ps_scale(ENV['APP_NAME'], 'worker', self.min_workers) unless self.jobs.count > 0 or self.workers == self.min_workers
+          unless self.jobs.count > 0 or self.workers == self.min_workers
+            @@mutex.sychronize do
+              client.post_ps_scale(ENV['APP_NAME'], 'worker', self.min_workers)
+              @workers = self.min_workers
+            end
+          end
         end
 
         def self.workers
-          client.get_ps(ENV['APP_NAME']).body.count { |p| p["process"] =~ /worker\.\d?/ }
+          @@mutex.synchronize do
+            @workers ||= Rails.cache.fetch("workless-workers", :expires_in => 15.minutes, :race_condition_ttl => 10.seconds) do
+              client.get_ps(ENV['APP_NAME']).body.count { |p| p["process"] =~ /worker\.\d?/ }
+            end
+          end
         end
 
         # Returns the number of workers needed based on the current number of pending jobs and the settings defined by:
