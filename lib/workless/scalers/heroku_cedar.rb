02c4a55b4b52f021777a0980eee4cf202e6ed6b7
Mark Wilson <mark@wilsonsdev.com>
2014-09-29 17:08:14 -0700

    fix bug returning proper workers count

--- lib/workless/scalers/heroku_cedar.rb
+++ lib/workless/scalers/heroku_cedar.rb
@@ -8,9 +8,9 @@ module Delayed
         @@mutex = Mutex.new
 
         def self.up
-          @@mutex.sychronize do
-            if self.workers_needed > self.min_workers and self.workers < self.workers_needed
-              client.post_ps_scale(ENV['APP_NAME'], 'worker', self.workers_needed) 
+          if self.workers_needed > self.min_workers and self.workers < self.workers_needed
+            client.post_ps_scale(ENV['APP_NAME'], 'worker', self.workers_needed) 
+            @@mutex.sychronize do
               @workers = self.workers_needed
             end
           end
@@ -18,8 +18,8 @@ module Delayed
 
         def self.down
           unless self.jobs.count > 0 or self.workers == self.min_workers
+            client.post_ps_scale(ENV['APP_NAME'], 'worker', self.min_workers)
             @@mutex.sychronize do
-              client.post_ps_scale(ENV['APP_NAME'], 'worker', self.min_workers)
               @workers = self.min_workers
             end
           end
@@ -27,7 +27,7 @@ module Delayed
 
         def self.workers
           @@mutex.synchronize do
-            @workers ||= Rails.cache.fetch("workless-workers", :expires_in => 15.minutes, :race_condition_ttl => 10.seconds) do
+            return @workers ||= Rails.cache.fetch("workless-workers", :expires_in => 15.minutes, :race_condition_ttl => 10.seconds) do
               client.get_ps(ENV['APP_NAME']).body.count { |p| p["process"] =~ /worker\.\d?/ }
             end
           end
