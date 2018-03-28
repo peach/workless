b081ef30cf979fbb6048f87ddf9a6f40d40cf381
Miguel Sancho <sanchojaf@gmail.com>
2018-03-28 13:01:12 -0700

    fixed error for rails 5 Passing string to define a callback is not supported

--- lib/workless/scaler.rb
+++ lib/workless/scaler.rb
@@ -12,28 +12,28 @@ module Delayed
         base.send :extend, ClassMethods
         if base.to_s =~ /ActiveRecord/
           base.class_eval do
-            after_commit "self.class.scaler.down", :on => :update, :if => Proc.new {|r| !r.failed_at.nil? }
-            after_commit "self.class.scaler.down", :on => :destroy, :if => Proc.new {|r| r.destroyed? or !r.failed_at.nil? }
-            after_commit "self.class.scaler.up", :on => :create
-          end          
+            after_commit :"self.class.scaler.down", :on => :update, :if => Proc.new {|r| !r.failed_at.nil? }
+            after_commit :"self.class.scaler.down", :on => :destroy, :if => Proc.new {|r| r.destroyed? or !r.failed_at.nil? }
+            after_commit :"self.class.scaler.up", :on => :create
+          end
         elsif base.to_s =~ /Sequel/
-          base.send(:define_method, 'after_destroy') do
+          base.send(:define_method, :'after_destroy') do
             super
             self.class.scaler.down
           end
-          base.send(:define_method, 'after_create') do
+          base.send(:define_method, :'after_create') do
             super
             self.class.scaler.up
           end
-          base.send(:define_method, 'after_update') do
+          base.send(:define_method, :'after_update') do
             super
             self.class.scaler.down
           end
         else
           base.class_eval do
-            after_destroy "self.class.scaler.down"
-            after_create "self.class.scaler.up"
-            after_update "self.class.scaler.down", :unless => Proc.new {|r| r.failed_at.nil? }
+            after_destroy :"self.class.scaler.down"
+            after_create :"self.class.scaler.up"
+            after_update :"self.class.scaler.down", :unless => Proc.new {|r| r.failed_at.nil? }
           end
         end
       end
